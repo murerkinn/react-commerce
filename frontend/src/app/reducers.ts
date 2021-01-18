c5d9f517e4d44edc3ae06749cc5ace0d55983346
Murat Erkin Çiçek <murerkinn@gmail.com>
2021-01-18 20:53:58 +0300

    Add reducers to store

--- frontend/src/app/reducers.ts
+++ frontend/src/app/reducers.ts
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux'
 import { userReducer } from '../login/login-store'
 import { userRegisterReducer } from '../register/register-store'
+import { productListReducer, productDetailsReducer } from '../product/product-store'
 
 const rootReducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
   user: userReducer,
   userRegisterReducer,
 })

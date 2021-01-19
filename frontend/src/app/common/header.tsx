9f04258c83e440a5dd12ce45ef72c5f02b6afeac
Murat Erkin Çiçek <murerkinn@gmail.com>
2021-01-19 14:00:48 +0300

    Update header component

--- frontend/src/app/common/header.tsx
+++ frontend/src/app/common/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { Navbar, Nav, Container, Button, NavDropdown } from 'react-bootstrap'
 import { logout } from '../../login/login-store'
 import { AppState } from '../store'
 
@@ -8,57 +9,43 @@ const Header: React.FC = () => {
   const dispatch = useDispatch()
   const { userInfo } = useSelector((state: AppState) => state.user)
 
-  let isLoggedIn = userInfo !== null
-
   const doLogout = () => {
     dispatch(logout())
   }
 
   return (
-    <header className="mb-5">
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            <strong>React Node Starter</strong>
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Link to="/">
+            <Navbar.Brand>RCommerece</Navbar.Brand>
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo02"
-            aria-controls="navbarTogglerDemo02"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav mb-2 mb-lg-0" style={{ marginLeft: 'auto' }}>
-              {!isLoggedIn && (
-                <li className="nav-item">
-                  <Link className="nav-link" to="/login">
-                    Login
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Link to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i> Cart
+                </Nav.Link>
+              </Link>
+              {userInfo !== null ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <Link to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
                   </Link>
-                </li>
+                  <NavDropdown.Item onClick={doLogout}>Logout</NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <Link to="/login">
+                  <Nav.Link>
+                    <i className="fas fa-user"></i> Login
+                  </Nav.Link>
+                </Link>
               )}
-              {!isLoggedIn && (
-                <li className="nav-item">
-                  <Link className="nav-link" to="/register">
-                    Register
-                  </Link>
-                </li>
-              )}
-              {isLoggedIn && (
-                <li className="nav-item">
-                  <button className="nav-link btn btn-link" onClick={doLogout}>
-                    Logout
-                  </button>
-                </li>
-              )}
-            </ul>
-          </div>
-        </div>
-      </nav>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   )
 }

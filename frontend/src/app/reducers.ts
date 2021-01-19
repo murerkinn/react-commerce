c694540fdfa2533c6fa41938f51fe0720762e8a9
Murat Erkin Çiçek <murerkinn@gmail.com>
2021-01-19 14:00:20 +0300

    Add cart reducer to reducers

--- frontend/src/app/reducers.ts
+++ frontend/src/app/reducers.ts
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 import { userReducer } from '../login/login-store'
 import { userRegisterReducer } from '../register/register-store'
 import { productListReducer, productDetailsReducer } from '../product/product-store'
+import { cartReducer } from '../cart/cart-store'
 
 const rootReducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
   user: userReducer,
   userRegisterReducer,
 })
